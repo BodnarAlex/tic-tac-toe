7fe43b236e649fcc7f6ac2be0caa755db7b3ef2c
Bodnar Alex <allugovskova@mail.ru>
2023-10-05 21:59:12 +0200

    feat: add script restart of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let tic = true;
+let game = [[],[],[]];
 
 window.addEventListener('load', () => {
 let click = document.getElementsByClassName("item");
@@ -7,13 +8,20 @@ console.log(click);
         click[i].onclick = function (e) {
             let elem = e.target.classList;
             if(!elem.contains('tic') && !elem.contains('tac')){
-                if(tic){
-                    elem.add('tic');  
-                } else{
-                    elem.add('tac');
-                }
+                (tic) ? elem.add('tic') : elem.add('tac');
                 tic = !tic;
             }
         };
     }
+    document.getElementById('restart').onclick = function(){
+        var tic = document.querySelectorAll('.tic');
+        for (var i = 0; i < tic.length; i++) {
+            tic[i].classList.remove('tic');
+        }
+        var tac = document.querySelectorAll('.tac');
+        for (var i = 0; i < tac.length; i++) {
+            tac[i].classList.remove('tac');
+        }
+    }
+
 });
