05e100c4228e0542256da67d5ce5cabe6ddff181
Bodnar Alex <allugovskova@mail.ru>
2023-10-06 20:34:36 +0200

    refactor: improve readable of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 let tic = true;
-let game = [[],[],[]];
+let game = [[], [], []];
 
 window.addEventListener('load', () => {
-let click = document.getElementsByClassName("item");
-console.log(click);
+    let click = document.getElementsByClassName("item");
+    console.log(click);
     for (var i = 0; i < click.length; i++) {
         click[i].onclick = function (e) {
             let elem = e.target.classList;
-            if(!elem.contains('tic') && !elem.contains('tac')){
+            if (!elem.contains('tic') && !elem.contains('tac')) {
                 (tic) ? elem.add('tic') : elem.add('tac');
                 tic = !tic;
             }
         };
     }
-    document.getElementById('restart').onclick = function(){
+    document.getElementById('restart').onclick = function () {
         var tic = document.querySelectorAll('.tic');
         for (var i = 0; i < tic.length; i++) {
             tic[i].classList.remove('tic');
@@ -23,5 +23,4 @@ console.log(click);
             tac[i].classList.remove('tac');
         }
     }
-
 });
