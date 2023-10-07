7229d11616ed6c0cb30f923775ef0c710a64983b
Bodnar Alex <allugovskova@mail.ru>
2023-10-07 22:22:00 +0200

    feat: add script check of win

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,40 @@
 let tic = true;
-let game = [[], [], []];
+let win = '';
+let ticSign = 'X';
+let tacSign = '0';
+let game =[];
+let gameCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
 window.addEventListener('load', () => {
+
     let click = document.getElementsByClassName("item");
-    console.log(click);
     for (var i = 0; i < click.length; i++) {
         click[i].onclick = function (e) {
             let elem = e.target.classList;
             if (!elem.contains('tic') && !elem.contains('tac')) {
-                (tic) ? elem.add('tic') : elem.add('tac');
+                if(tic){
+                    elem.add('tic');
+                    game[e.target.id-1]='X';
+                }else{
+                    elem.add('tac');
+                    game[e.target.id-1]='0';
+                }
                 tic = !tic;
+                checkWin('X');
+                checkWin('0');
             }
         };
     }
+
     document.getElementById('restart').onclick = function () {
         var tic = document.querySelectorAll('.tic');
         for (var i = 0; i < tic.length; i++) {
@@ -22,5 +44,12 @@ window.addEventListener('load', () => {
         for (var i = 0; i < tac.length; i++) {
             tac[i].classList.remove('tac');
         }
+        game = [];
     }
 });
+
+function checkWin(item){
+    let map = gameCombinations.map((array) => array.every((elem) => game[elem] == item));
+    console.log(map.indexOf(true));
+    return map.indexOf(true);
+}
